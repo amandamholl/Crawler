e6528f29c7cfda913f66e6ee5b226ce97904a79d
Amanda <aholl@scu.edu>
2016-04-19 14:52:04 -0700

    Fix politeness policy

--- src/com/example/crawler/Crawler.java
+++ src/com/example/crawler/Crawler.java
@@ -2,15 +2,12 @@ package com.example.crawler;
 
 import java.io.*;
 
-import java.net.MalformedURLException;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
-import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -32,6 +29,8 @@ public class Crawler {
     private static List<String> pages_to_crawl = new LinkedList<String>();
     private static int max_pages;
     private static String prev_page = "";
+    private static ConcurrentSkipListSet<String> hosts_visited = new ConcurrentSkipListSet<String>();
+    private static Timer remove;
 
     public static void main(String[] args){
 
@@ -42,7 +41,7 @@ public class Crawler {
         if(repository.exists()){
             String[] entries = repository.list();
             for(String s: entries){     // Delete all pages in repository
-                System.out.println("here");
+                //System.out.println("here");
                 File current = new File(repository.getPath(),s);
                 current.delete();
             }
@@ -76,6 +75,19 @@ public class Crawler {
             if(seed[2] != null) // If it is, capture it
                 domain_restriction = seed[2];
             System.out.println(domain_restriction);
+
+            TimerTask removeDomains = new TimerTask(){
+                @Override
+                public void run() {
+                    System.out.println("\n HERE \n");
+                    if(!hosts_visited.isEmpty())
+                        hosts_visited.remove(hosts_visited.pollLast());
+                }
+            };
+
+            remove = new Timer("MyTimer");
+            remove.scheduleAtFixedRate(removeDomains, 0, 1000);
+
 
             /*  Start crawling with the specifications pulled from specifications.csv */
             crawl(seed[0], seed[2]);
@@ -118,53 +130,37 @@ public class Crawler {
     public static void crawl(String URL, String domain_restriction){
         while(pages_crawled.size() < max_pages){
             String current_page;
-            if(pages_to_crawl.isEmpty()){   // Set of pages to crawl is empty
-                current_page = URL;
-                pages_crawled.add(URL);
-            }
-            else{
+
+            if(!pages_to_crawl.isEmpty()){  // If there are pages to crawl
                 do{
                     current_page = pages_to_crawl.remove(0);
                 }while(pages_crawled.contains(current_page));
-                pages_crawled.add(current_page);
             }
+            else
+                current_page = URL;
+
+            /*  If the domain has been visited in the last second, don't crawl this page */
+            if(!checkDomain(current_page)) {
+                System.out.println("BE POLITE");
+                pages_to_crawl.add(current_page);   // Add the page back in the list of pages to crawl b/c politeness protocol says we can't right now
+                continue;
+            }
+
+            pages_crawled.add(current_page);
+
             System.out.println(current_page + " "+ prev_page);
 
-            /*  If crawling pages from the same domain, introduce delay to be polite.
-             *  Otherwise, just crawl the page without waiting.
+            /*  Crawling this page is disallowed by site's robots.txt
+             *  Return and do not crawl.
              */
-
-            //has to be better way to do this
-
-            checkRobots(current_page);
-
-            String domain = "";
-            try {
-                domain = new URI(prev_page).getHost();
-                System.out.println(domain);
-            }
-            catch(URISyntaxException e){
-                System.out.println("Error getting host");
+            if(!checkRobots(current_page)){
+                continue;
             }
 
-            if(domain != null && current_page.contains(domain)){
-                System.out.println("Be polite");
-
-                /*  Delay crawl by 1s */
-                try {
-                    TimeUnit.SECONDS.sleep(1);
-                    getPage(current_page, domain_restriction);
-                }
-                catch(InterruptedException e){
-                    Thread.currentThread().interrupt();
-                }
-
-            }
-            else {
-                getPage(current_page, domain_restriction);
-            }
-            prev_page = current_page;   // Update record of the page previously crawled
+            getPage(current_page, domain_restriction);
         }
+        System.out.println("\n\nDone-----------------");
+        remove.cancel();
     }
 
     /*  Check robots will open up the robots.txt file for the URL's host
@@ -175,7 +171,7 @@ public class Crawler {
         String host = "";
         try {
             host = new URI(URL).getHost();
-            System.out.println("Host is: "+ host);
+            //System.out.println("Host is: "+ host);
         }
         catch(URISyntaxException e){
             System.out.println("Error getting host");
@@ -189,13 +185,54 @@ public class Crawler {
             System.out.println("Could not create URL for robots.txt");
             return false;
         }
-        System.out.println(robots_url.toString());
+        //System.out.println(robots_url.toString());
+
+        URL url;
+        try{
+            url = new URL(URL);
+        }
+        catch(MalformedURLException e){
+            System.out.println("Could not create URL for page");
+            return false;
+        }
 
         try{
             BufferedReader robots_stream = new BufferedReader(new InputStreamReader(robots_url.openStream()));
             String line = "";
+            String disallow = "Disallow:";
+            String page = url.getFile();
+
             while((line = robots_stream.readLine()) != null){
                 //System.out.println(line);
+                int offset = 0;
+
+                /*   Check if the line is a disallow statement we need to follow */
+                if(line.indexOf(disallow, offset) != -1){
+
+                    offset += disallow.length();
+
+                    /*  Tokenize the line read, starting after the phrase "Disallow:"
+                     *  to get the directory banned
+                     */
+                    String path = line.substring(offset);
+                    StringTokenizer tokens = new StringTokenizer(path);
+
+                    if(tokens.hasMoreTokens()){
+                        String forbidden = tokens.nextToken();
+
+                        //System.out.println("Forbidden " + forbidden);
+
+                        /*  Check that the URL we want to crawl is not in the banned
+                         *  directory
+                         */
+                        if(page.indexOf(forbidden) == 0){
+                            System.out.println("CRAWL FORBIDDEN on this page");
+                            return false;
+                        }
+                    }
+
+
+                }
                 // Get the folder disallowed
                 // if url is in there, don't crawl, return false
             }
@@ -206,6 +243,30 @@ public class Crawler {
         }
 
         return true;
+    }
+
+    public static boolean checkDomain(String URL){
+        String domain = "";
+        try {
+            domain = new URI(URL).getHost();
+            System.out.println(domain);
+        }
+        catch(URISyntaxException e){
+            System.out.println("Error getting host");
+            return false;
+        }
+
+        /*  Check if crawling a page from a domain visited within the last second.
+         *  If so, do not crawl. Otherwise, go ahead and crawl.
+         */
+        if(hosts_visited.isEmpty() || !hosts_visited.contains(domain)) { // If domain not in list of domains visited
+            hosts_visited.add(domain);  // Add it
+            System.out.println("\n"+ hosts_visited+ "\n");
+        }else
+            return false;   // Don't crawl this page
+
+        return true;
+
     }
 
     public static void getPage(String URL, String domain_restriction){

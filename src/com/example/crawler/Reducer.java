8a6757a3b0e42098b9636aa84d30b9313ebdfa1d
Paige Rogalski <progalski@soasta.com>
2016-04-24 17:09:26 -0700

    Updates to Reducer.java

--- src/com/example/crawler/Reducer.java
+++ src/com/example/crawler/Reducer.java
@@ -2,27 +2,24 @@ package src.com.example.crawler;
 
 import java.io.*;
 import java.util.HashSet;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 /*
- * Want to keep text between <a> and </a> and 
  * Keep meta content?
- * Anything inside <p> (already handles <a> inside p)
- * Figure out how to not include nav bars / footers 
  * <p>&nbsp;</p>  don't put that in text 
  * 
- * Don't put anything in <ul> tags 
  * 
- * duplicates?
  */
 public class Reducer {
 	private static File textRepo = new File("textRepo");
 	private static File htmlRepo = new File("/Users/progalski/Documents/Crawler/Crawler/repository");
-	private static File stopWords = new File("/Users/progalski/Documents/Crawler/Crawler/stopwords.txt");
+	private static File stopWords = new File("/Users/progalski/Documents/Crawler/Crawler/stopwords_en.txt");
 	private static HashSet<Element> excludeElements = new HashSet<>();
 
 	public static void main(String[] args) {
@@ -58,8 +55,11 @@ public class Reducer {
 		BufferedWriter bw;
 
 		String[] entries = htmlRepo.list();
-		for (String s : entries) {
-			File current = new File(htmlRepo.getPath(), s);
+		//for (String s : entries) {
+			//File current = new File(htmlRepo.getPath(), s);
+			//File current = new File(htmlRepo.getPath(), "TRS.htm");
+			File current = new File(htmlRepo.getPath(), "CNN.htm");
+
 
 			try {
 
@@ -75,72 +75,115 @@ public class Reducer {
 
 				bw = new BufferedWriter(new FileWriter(outputFile));
 
-				// Get the elements we do not want to write to the text file
-				// Element header = doc.getElementById("header");
-				// if(header != null) {
-				// //System.out.println(header.text());
-				// excludeElements.add(header);
-				// }
-				//
-				// Element footer = doc.getElementById("footer");
-				// if(footer != null) {
-				// //System.out.println(footer.text());
-				// excludeElements.add(footer);
-				// }
-
-				// get <tr> and <ul> tags and put them in exclude elements
+				 //Get the elements we do not want to write to the text file
+				 Elements headers = doc.select("div[id~=header], div[class~=header]");
+				 for(Element head : headers) {
+					 head.attr("written", false);
+					 excludeElements.add(head);
+				 }
+				 
+				 Elements footers = doc.select("div[id~=foot], div[class~=foot]");
+				 for(Element foot : footers) {
+					 foot.attr("written", false);
+					 excludeElements.add(foot);
+				 }
+				
+				Elements navs = doc.select("div[id~=nav], div[class~=nav], nav");	//gets any div with id containing nav
+				for(Element nav : navs)
+				{
+					nav.attr("written", false);
+					excludeElements.add(nav);
+				}
+				
+				//handle ads 
+				Elements ads = doc.select("div[id~=ad], div[class~=ad]");
+				for(Element ad : ads)
+				{
+					excludeElements.add(ad);
+				}
+				
+				
 				boolean canWrite = true;
-				// TODO: doesn't work ends up getting the p with id=footlinks
-				// problem with nested DIVS
+				
+				//Hanlde divs with text 
 				Elements divs = doc.getElementsByTag("div");
-				for (Element div : divs) {
-					if (!div.id().equals("header") && !div.id().equals("footer")) {
-						Elements pgraphs = div.getElementsByTag("p");
-						for (Element pgraph : pgraphs) {
-							Elements parents = pgraph.parents();
-							for (Element parent : parents) {
-								if (parent.id().equals("header") || parent.id().equals("footer")) {
-									// if the <p> element's parent is not header
-									// or footer then can write it to file
-									canWrite = false;
-									break;
-								}
-							}
-							if (canWrite) {
-								String pText = removeStopWords(pgraph.text());
-								System.out.println(pText);
-								bw.write(pText);
-								bw.newLine();
+				for(Element div : divs) {
+					if(!excludeElements.contains(div))
+					{
+						Elements parents = div.parents();
+						for (Element parent : parents) {
+							if (excludeElements.contains(parent) || parent.hasAttr("written")) {
+								// if the <div> element's parent is header or footer do not write it to file
+								canWrite = false;
+								div.attr("written", false);
+								break;
 							}
 						}
 					}
+					if (canWrite) {
+						div.attr("written", true);
+						String divText = removeStopWords(div.text());
+						bw.write(divText);
+						bw.newLine();
+					}
 				}
-
-				// for(Element div : divs) {
-				// if(!div.id().equals("header") && !div.id().equals("footer"))
-				// {
-				// Elements divKids = div.children();
-				// for(Element divKid : divKids) {
-				// if(!divKid.id().equals("header") &&
-				// !divKid.id().equals("footer"))
-				// {
-				//
-				// Elements pgraphs = divKid.getElementsByTag("p");
-				// for (Element pgraph : pgraphs) {
-				// String pText = removeStopWords(pgraph.text());
-				// System.out.println(pText);
-				// bw.write(pText);
-				// bw.newLine();
-				//
-				// }
-				// }
-				// }
-				// }
-				// }
-				//
-				//
-				//
-
+						
+				Elements pgraphs = doc.getElementsByTag("p");
+				for (Element pgraph : pgraphs) {
+					if(!excludeElements.contains(pgraph)) {
+						Elements parents = pgraph.parents();
+						for (Element parent : parents) {
+							if (excludeElements.contains(parent) || parent.hasAttr("written")) {
+								// if the <p> element's parent is header or footer do not write it to file
+								canWrite = false;
+								break;
+							}
+						}
+						if (canWrite) {
+							String pText = removeStopWords(pgraph.text());
+							bw.write(pText);
+							bw.newLine();
+						}
+					}
+				}
+				
+				//Handle list items - we want to include these because list are usually important
+				// example is in Wikipedia references, those are usually relevant to the page
+				canWrite = true;
+				Elements listElements = doc.getElementsByTag("li");
+				for(Element listItem : listElements) {
+					//need to check if parent has already been written 
+					Elements listParents = listItem.parents();
+					for(Element parent : listParents) {
+						if(excludeElements.contains(parent) || parent.hasAttr("written")) {
+							canWrite = false;
+							break;
+						}
+					}
+					if(canWrite) {
+						bw.write(listItem.text());
+						bw.newLine();
+					}
+				}
+				
+				//Handle img alt tags 
+				canWrite = true;
+				Elements imgElements = doc.getElementsByTag("img");
+				for(Element img : imgElements) {
+					Elements imgParents = imgElements.parents();
+					for(Element parent : imgParents) {
+						if(excludeElements.contains(parent) || parent.hasAttr("written")) {
+							canWrite = false;
+							break;
+						}
+					}
+					if(canWrite) {
+						bw.write(img.attr("alt"));
+						bw.newLine();
+					}
+				}
+					
+							
 				bw.close();
 
 			} catch (FileNotFoundException e) {
@@ -150,7 +193,7 @@ public class Reducer {
 				System.out.println("Error reading HTML file.");
 				return;
 			}
-		}
+		//}
 
 	}
 
@@ -160,10 +203,7 @@ public class Reducer {
 			br = new BufferedReader(new FileReader(stopWords));
 			String line;
 			while ((line = br.readLine()) != null) {
-				// System.out.println(text);
-				// line.replaceAll("\\n", "");
-				// System.out.println(line);
-				// text.replace(line, "");
+				text.replaceAll(line, "");
 			}
 			br.close();
 			return text;
